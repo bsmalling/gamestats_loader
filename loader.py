b469f75d189dfc781c3f7910657a495fc87ac8b0
Brad Smalling <bsmalling@users.noreply.github.com>
2022-09-05 10:24:34 -0500

    Deleted my first attempts and renamed the new versions to replace.

--- loader.py
+++ loader.py
@@ -5,206 +5,165 @@ from dateutil import parser as dtparser
 import pymysql
 import csv
 import sys
+import getopt
 import os
+import re
 
-def format_column_values(values):
-    return [ "NULL" if value == '' else "'" + value + "'" for value in values ]
+verbose = False
 
-def format_column_names(columns):
-    return [ "`" + column + "`" for column in columns ]
+class MySQLTableLoader:
+    """
+    A class used to fill a MySQL table from corresponding CSV data.
+    """
 
-def load_match_overview(uri, reader):
-    columns = [
-        #"match_key",
-        "match_id",
-        "match_time",
-        "team1_name",
-        "team2_name",
-        "game_mode",
-        "map_name",
-        "winner_name",
-        "team1score",
-        "team2score",
-        "atk_at_start",
-        "team1_atk_wins",
-        "team1_def_wins",
-        "team2_atk_wins",
-        "team2_def_wins",
-        "team1_score_at_half",
-        "team2_score_at_half"
-    ]
-    query_columns = ",".join(format_column_names(columns))
+    def __init__(self, table_name, engine):
+        self._table_name = table_name
+        results = engine.execute(f"DESCRIBE `{table_name}`")
+        self._column_names = list()
+        self._column_info = dict()
+        for col_name, col_type, nullable, key, default, extra in results:
+            self.column_names.append(col_name)
+            col_type = re.search("(\w+)", col_type).group()
+            extra = ("auto_increment" in extra)
+            self._column_info[col_name] = (col_type, extra)
 
-    engine = sa.create_engine(uri)
-    row = next(reader)
-    while len(row) > 2:
-        values = row[1:]
-        # Format match_time expression for MySql datetime
-        values[1] = dtparser.parse(values[1]).strftime("%Y-%m-%d %H:%M:%S")
-        values = ",".join(format_column_values(values))
+    @property
+    def table_name(self):
+        return self._table_name
 
-        query = "INSERT INTO `matches` (%s) VALUES (%s)" % (query_columns, values)
-        result = engine.execute(query)
-        match_key = result.lastrowid
+    @property
+    def column_names(self):
+        return self._column_names
+
+    @property
+    def column_info(self):
+        return self._column_info
+
+    def load(self, reader, engine, key=None):
+        if verbose:
+            print(f"Loading table {self._table_name}...")
+
+        auto_inc = self._column_info[self._column_names[0]][1]
+
+        row_count = 0
+        match_key = None
         row = next(reader)
+        # This data is really shitty and unreliable. Serious amateutr crap...
+        # Some rows contain meaningless values of 1,2,3,4.... We have3 to ignore these rows. Hence -10.
+        while len(row) > len(self._column_names) - 10:
+            if auto_inc:
+                values = row[1:]
+            else:
+                values = row[2:]
 
-    return match_key
+            if key != None:
+                values.insert(0, str(key))
 
-def load_match_performance(uri, reader, match_key):
-    columns = [
-        "match_key",
-        "team_name",
-        "player_name",
-        "player_rating",
-        "atk_rating",
-        "def_rating",
-        "k_d",
-        "entry",
-        "trade_diff",
-        "kost",
-        "kpr",
-        "srv",
-        "hs",
-        "atk_op",
-        "def_op",
-        "kills",
-        "refrags",
-        "headshots",
-        "underdog_kills",
-        "onevx",
-        "multikill_rounds",
-        "deaths",
-        "traded_deaths",
-        "traded_by_enemy",
-        "opening_kills",
-        "opening_deaths",
-        "entry_kills",
-        "entry_deaths",
-        "planted_defuser",
-        "disabled_defuser",
-        "teamkills",
-        "teamkilled",
-        "ingame_points",
-        "last_alive"
-    ]
-    query_columns = ",".join(format_column_names(columns))
+            table_columns = self._format_column_names()
+            table_values = self._format_column_values(values) 
+            query = "INSERT INTO `%s` (%s) VALUES (%s)" % (self._table_name, table_columns, table_values)
+            #print(query)
+            result = engine.execute(query)
+            row_count += 1
+            if auto_inc:
+                match_key = result.lastrowid
+            row = next(reader)
 
-    engine = sa.create_engine(uri)
-    row = next(reader)
-    while len(row) > 30:
-        values = row[2:-1]
-        # Fix kost value of "NaN"
-        values[8] = "" if values[8] == "NaN" else values[8]
-        # Fix hs percentage expression for MySql
-        values[11] = str(float(values[11][:-1]) / 100.0)
-        # Fix last_alive value of "undefined"
-        values[32] = "" if values[32] == "undefined" else values[32]
-        values = ",".join(format_column_values(values))
+        if verbose:
+            print(f"Loaded {row_count} rows.")
+        return match_key
 
-        query = "INSERT INTO `performance` (%s) VALUES (%d,%s)" % (query_columns, match_key, values)
-        engine.execute(query)
-        row = next(reader)
+    def _format_column_names(self):
+        sql_columns = []
+        for col_name in self._column_names:
+            _, auto_inc = self._column_info[col_name]
+            if auto_inc:
+                continue
+            else:
+                sql_columns.append("`" + col_name + "`")
+        return ",".join(sql_columns)
 
-def load_match_player_rounds_data(uri, reader, match_key):
-    columns = [
-        "match_key",
-        "team_name",
-        "player_name",
-        "round",
-        "map",
-        "site",
-        "side",
-        "result",
-        "round_time",
-        "victory_type",
-        "operator",
-        "time_spent_alive",
-        "kills",
-        "refrags",
-        "headshots",
-        "underdog_kills",
-        "onevx",
-        "death",
-        "traded_death",
-        "refragged_by",
-        "traded_by_enemy",
-        "opening_kill",
-        "opening_death",
-        "entry_kill",
-        "entry_death",
-        "planted_defuser",
-        "disabled_defuser",
-        "teamkills",
-        "teamkilled",
-        "ingame_points",
-        "last_alive",
-        "drones_deployed",
-        "drone_survived_prep_phase",
-        "drone_found_bomb",
-        "total_droning_distance",
-        "total_time_drone_piloting",
-        "yellow_ping_leading_to_kill"
-    ]
-    query_columns = ",".join(format_column_names(columns))
+    def _format_column_values(self, values):
+        while len(values) < len(self._column_names):
+            values.append('')
 
-    engine = sa.create_engine(uri)
-    row = next(reader)
-    while len(row) > 2:
-        values = row[2:-1]
-        # Fix time_spent_alive when set to "-"
-        values[10] = "" if values[10] == "-" else values[10]
-        # Fix ingame_points when set to "undefined"
-        values[28] = "" if values[28] == "undefined" else values[28]
-        # Fix boolean values
-        if len(values) > 34:
-            values[31] = "1" if values[31] == "true" else "0"
-            values[32] = "1" if values[32] == "true" else "0"
-            values[35] = "1" if values[35] == "true" else "0"
-        # Empty values at the end of the table are not read from csv
-        while len(values) < len(columns) - 1:
-             values.append('')
-        values = ",".join(format_column_values(values))
+        index = 0
+        sql_values = []
+        for col_name in self._column_names:
+            value = values[index]
 
-        query = "INSERT INTO `player_rounds` (%s) VALUES (%d,%s)" % (query_columns, match_key, values)
-        engine.execute(query)
-        row = next(reader)
+            col_type, auto_inc = self._column_info[col_name]
+            if auto_inc:
+                continue
+            index += 1
 
-def load_match_round_events_breakdown(uri, reader, match_key):
-    columns = [
-        "match_key",
-        "round",
-        "time_into_round",
-        "vod_time",
-        "team_name",
-        "type",
-        "actor_name",
-        "victim_name",
-        "blue_team_alive",
-        "orange_team_alive",
-        "kill_subtype"
-    ]
-    query_columns = ",".join(format_column_names(columns))
+            if col_type == "int":
+                if value == "" or value == "-" or value == "undefined":
+                    sql_values.append("NULL")
+                else:
+                    sql_values.append(value)
+                continue
 
-    engine = sa.create_engine(uri)
-    row = next(reader)
-    while len(row) > 2:
-        values = row[2:]
-        # Fix time_into_round value of "NaN"
-        values[1] = "0" if values[1] == "NaN" else values[1]
-        # Fix vod_time value of "aN:aN"
-        values[2] = "00:00" if values[2] == "aN:aN" else values[2]
-        values = ",".join(format_column_values(values))
+            if col_type == "bigint":
+                if value == "" or value == "-" or value == "undefined" or value == "NaN":
+                    sql_values.append("NULL")
+                else:
+                    sql_values.append(value)
+                continue
 
-        query = "INSERT INTO `round_events` (%s) VALUES (%d,%s)" % (query_columns, match_key, values)
-        engine.execute(query)
-        row = next(reader)
+            if col_type == "float":
+                if value == "" or value == "NaN":
+                    sql_values.append("NULL")
+                elif value[-1] == "%":
+                    sql_values.append(str(float(value[:-1]) / 100.0))
+                else:
+                    sql_values.append(value)
+                continue
+               
+            if col_type == "varchar":
+                if (value == "-"):
+                    sql_values.append("NULL")
+                else:
+                    sql_values.append("'" + value + "'")
+                continue
 
-def do_load(uri, filename):
+            if col_type == "datetime":
+                mysql_date = dtparser.parse(value).strftime("%Y-%m-%d %H:%M:%S")
+                sql_values.append("'" + mysql_date + "'")
+                continue
+
+            if col_type == "time":
+                if value == "aN:aN":
+                    sql_values.append("NULL")
+                else:
+                    sql_values.append("'" + value + "'")
+                continue
+
+            if col_type == "tinyint" or col_type == "boolean":
+                if value == "":
+                    sql_values.append("NULL")
+                else:
+                    sql_values.append("1" if value == "true" else "0")
+                continue
+
+            raise NotImplementedError(f"Unknown column type: {col_type}.")
+
+        return ",".join(sql_values)
+
+def do_load(engine, filename):
+    if verbose:
+        print(f"Loading {filename}...")
+
+    matches = MySQLTableLoader("matches", engine)
+    performance = MySQLTableLoader("performance", engine)
+    player_rounds = MySQLTableLoader("player_rounds", engine)
+    round_events = MySQLTableLoader("round_events", engine)
+
     fh = open(filename)
     try:
         reader = csv.reader(fh)
         row = next(reader)
-        match_key = -1
+        match_key = None
         while True:
             if len(row) <= 1:
                 row = next(reader)
@@ -213,24 +172,26 @@ def do_load(uri, filename):
             label = row[1]
             if label == "MATCH OVERVIEW":
                 next(reader) # Skip header
-                match_key = load_match_overview(uri, reader)
+                match_key = matches.load(reader, engine)
             elif label == "MATCH PERFORMANCE":
                 next(reader) # Skip header
-                load_match_performance(uri, reader, match_key)
+                performance.load(reader, engine, match_key)
             elif label == "PLAYER ROUNDS DATA":
                 next(reader) # Skip header
-                load_match_player_rounds_data(uri, reader, match_key)
+                player_rounds.load(reader, engine, match_key)
             elif label == "ROUND EVENTS BREAKDOWN":
                 next(reader) # Skip header
-                load_match_round_events_breakdown(uri, reader, match_key)
+                round_events.load(reader, engine, match_key)
                 break
 
             row = next(reader)
     finally:
         fh.close()
 
-def do_reset(uri):
-    engine = sa.create_engine(uri)
+def do_reset(engine):
+    if verbose:
+        print("Resetting database...")
+
     engine.execute("SET FOREIGN_KEY_CHECKS = 0")
     engine.execute("TRUNCATE TABLE `round_events`")
     engine.execute("TRUNCATE TABLE `player_rounds`")
@@ -238,28 +199,49 @@ def do_reset(uri):
     engine.execute("TRUNCATE TABLE `matches`")
     engine.execute("SET FOREIGN_KEY_CHECKS = 1")
 
+    if verbose:
+        print("Database reset.")
+
 def show_help():
-    print("loader.py [command] [arguments...]")
-    print()
-    print("Commands:")
-    print("  LOAD [filename]")
-    print("  RESET")
-    print("  HELP")
+    print("loader2.py [arguments...]")
+    print("Arguments:")
+    print(str.ljust("  -l or --load [filename]", 30) + "Load [filename]")
+    print(str.ljust("  -r or --reset", 30) + "Reset database")
+    print(str.ljust("  -v or --verbose", 30) + "Verbose")
+    print(str.ljust("  -h or --help", 30) + "Show help")
 
 def main():
+    global verbose
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "l:rvh", ["load=", "reset", "verbose", "help"])
+    except getopt.GetoptError:
+        show_help()
+        sys.exit(2)
+
+    reset = False
+    filename = None
+    for opt, arg in opts:
+        if opt in ("-l", "--load"):
+            filename = arg
+        elif opt in ("-r", "--reset"):
+            reset = True
+        elif opt in ("-v", "--verbose"):
+            verbose = True
+        elif opt in ("-h", "--help"):
+            show_help()
+            sys.exit(0)
+
     uri = "mysql+pymysql://appuser:%s@localhost/gamestats" % os.environ["GAMESTATS_APPUSER_PWD"]
+    engine = sa.create_engine(uri)
 
-    command = sys.argv[1].lower()
+    if reset:
+        do_reset(engine)
 
-    if command == "load":
-        do_load(uri, sys.argv[2])
-    elif command == "reset":
-        do_reset(uri)
-    elif command == "help":
-        show_help()
-    else:
-        print("Invalid command.")
-        show_help()
+    if filename != None:
+        do_load(engine, filename)
+
+    engine.dispose()
 
 if __name__ == "__main__":
     main()
--- loader2.zsh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-python loader2.py --verbose --reset
-find ../gamestats_data -type f -name \*.csv -exec python loader2.py --verbose --load '{}' \;
