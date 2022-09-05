190ebcf89fbc70472e3d6dbad5250497dab12f4f
Brad Smalling <bsmalling@users.noreply.github.com>
2022-09-05 10:43:54 -0500

    Renamed loader2.py to loader.py (old version was replaced)

--- loader.zsh
+++ loader.zsh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-python loader2.py --verbose --reset
-find ../gamestats_data -type f -name \*.csv -exec python loader2.py --verbose --load '{}' \;
+python loader.py --verbose --reset
+find ../gamestats_data -type f -name \*.csv -exec python loader.py --verbose --load '{}' \;
