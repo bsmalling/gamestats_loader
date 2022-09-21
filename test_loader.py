3e7210496166367bda9687ac8331d2a97dda7908
Brad Smalling <bsmalling@users.noreply.github.com>
2022-09-21 14:35:18 -0500

    Added skeleton unit test for MySQLTableLoader. To be completed...

---
+++ test_loader.py
@@ -0,0 +1,36 @@
+import unittest
+from unittest.mock import Mock
+from loader import MySQLTableLoader
+
+# To be completed...
+
+class TestMySQLTableLoader(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_func(self):
+        # self.assertEqual(self.func(1), 'value1')
+        # self.assertEqual(self.func(2), 'value2')
+        pass
+
+    def test_func_raise(self):
+        #with self.assertRaises(ValueError):
+        #   self.func(0)
+        pass
+
+# end class TestMySQLTableLoader
+
+if __name__ == '__main__':
+    unittest.main()
